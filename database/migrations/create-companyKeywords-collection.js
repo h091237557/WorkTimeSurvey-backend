3605fa1bd16b1d0bf1aec4be38283e81e7b07149
mark <h091237557@gmail.com>
2017-07-07 11:13:03 +0800

    Modify: modify the capped collection

--- database/migrations/create-companyKeywords-collection.js
+++ database/migrations/create-companyKeywords-collection.js
@@ -1,8 +1,8 @@
 module.exports = (db) => {
     return db.createCollection("company_keywords", {
         capped: true,
-        size: 300000,
-        max: 1000,
+        size: 6000000,
+        max: 5000,
     })
     .then(() => {
         return db.collection('company_keywords').createIndex({
